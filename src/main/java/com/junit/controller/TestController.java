b5226af2036e9d9aed48c49f984b382f95dbe5f0
vishgarad <vishgarad12@gmail.com>
2019-08-22 11:33:57 +0530

    second commit

--- src/main/java/com/junit/controller/TestController.java
+++ src/main/java/com/junit/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.junit.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,11 @@ public class TestController {
 		emp.setSalary(3000);
 
 		return emp;
+	}
+	@GetMapping("/hello")
+	public String getHello()
+	{
+		return "hello";
 	}
 
 }
